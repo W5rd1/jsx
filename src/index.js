b232639c4aac4061bcbf6c7df1383653fdea5879
michael eldred <mickyfin10@hotmail.com>
2019-02-04 11:54:18 +0000

    name input with react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+// Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Create a React Component
+const App = () => {
+    return (
+        <div>
+            <label class="label" for="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{ backgroundColor: 'blue', color: 'white'}}>Submit</button>
+        </div>
+    );
+};
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+) 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Take the react component and display
