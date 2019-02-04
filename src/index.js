6e4872ae8f88801835df86c6b703338e76671841
michael eldred <mickyfin10@hotmail.com>
2019-02-04 12:24:48 +0000

    finished jsx excersize

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom';
 // Create a React Component
 const App = () => {
     const buttonText = { text: 'Click me'};
+    const labelText = "Enter name:";
     return (
         <div>
-            <label className="label" for="name">Enter name:</label>
+            <label className="label" htmlFor="name">{labelText}</label>
             <input id="name" type="text"/>
             <button style={{ backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
         </div>
